dc2d122d1c7866ff20c8e17424f5a061afda5326
aktepeayse96 <aktepeayse96@gmail.com>
2022-11-24 20:17:48 +0300

    24 kasım 2.commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginModel } from '../models/loginModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  apiUrl = 'https://localhost:7136/api/auth';
+  apiUrl = 'https://localhost:7136/api/auth/';
   constructor(private httpClient:HttpClient) { }
 
   login(loginModel:LoginModel){
-    return this.httpClient.post(this.apiUrl+"login",loginModel)
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login",loginModel)
   }
 
 
