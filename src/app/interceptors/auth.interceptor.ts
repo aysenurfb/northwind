c79066c7fb00b70295d5333934e295d9a9e84f9f
aktepeayse96 <aktepeayse96@gmail.com>
2022-11-27 19:06:56 +0300

    27 kasım commit 3

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -12,12 +12,12 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    let token = localStorage.getItem("token");
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = JSON.parse(localStorage.getItem('token'));
     let newRequest : HttpRequest<any>;
     newRequest = request.clone({
-      headers : request.headers.set("Authorization","Bearer " + token)
-    }) 
+      headers : request.headers.set("Authorization","Bearer " + token),
+    });
     return next.handle(request);
   }
 }
